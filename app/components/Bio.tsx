4e332b19ef2a8e98f188b9ef13b932482570aa39
hrvoje vincek <vincekhrvoje@gmail.com>
2024-09-04 11:50:31 +0200

    bio 2

--- app/components/Bio.tsx
+++ app/components/Bio.tsx
@@ -4,7 +4,11 @@ export default function Bio() {
       I&apos;m a enthusiastic software engineer driven by creating exceptional
       digital experiences. With a strong foundation in both technology and
       business operations, I excel at bridging the gap between innovative ideas
-      and practical solutions.
+      and practical solutions. <br />
+      My background as a coffee roaster manager instilled in me a deep
+      appreciation for efficiency and alignment with strategic goals. This
+      unique perspective allows me to quickly grasp project requirements and
+      deliver impactful results.
     </p>
   );
 }
